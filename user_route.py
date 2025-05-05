865f43feae7aa6c4c26b0240f3ab068b7c6d9d0a
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2025-05-05 16:08:05 +0530

    remove try catch

--- user_route.py
+++ user_route.py
@@ -12,59 +12,46 @@ print("Router defined")
 
 @router.post('/')
 def add_user(username: str, name: str, db: Session = Depends(get_db)):
-  try:
-    user = db.query(User).filter(User.username == username).first()
-    if user is None:
-      raise HTTPException(status_code=400, detail="User already existes, can't register")
-    user = User(username = username, name = name)
-    db.add(user)
-    db.commit()
-    return user
-  except Exception as err:
-    print(err)
-    raise HTTPException(status_code=500, detail="Internal Server Error, something went wrong")
+  user = db.query(User).filter(User.username == username).first()
+  if user:
+    raise HTTPException(status_code=400, detail="User already existes, can't register")
+  user = User(username = username, name = name)
+  db.add(user)
+  db.commit()
+  return user
 
 @router.get('/')
 def get_all_users(db: Session = Depends(get_db)):
-  try:
-    users = db.query(User).all()
-    return users
-  except:
-    raise HTTPException(status_code=500, detail="Internal Server Error, something went wrong")
+  users = db.query(User).all()
+  return users
   
 @router.get('/{id}')
 def get_user(id: int, db: Session = Depends(get_db)):
-  try:
-    user = db.query(User).filter(User.id == id).first()
-    if not user:
+  user = db.query(User).filter(User.id == id).first()
+  if not user:
       raise HTTPException(status_code=400, detail="User not found. Invalid UserID")
-    return user
-  except:
-    raise HTTPException(status_code=500, detail="Internal Server Error, something went wrong")
+  return user
   
 @router.put('/{id}')
-def update_user(id: int, name: str, username: str, db : Session = Depends(get_db)):
-  try:
-    user = db.query(User).filter(User.id == id).first()
-    if not user:
-      raise HTTPException(status_code=418, detail="User not found. Invalid UserID")
-    if name:
-      user.name = name
-    if username:
+def update_user(id: int, name: str, username: str, db: Session = Depends(get_db)):
+  user = db.query(User).filter(User.id == id).first()
+  if not user:
+      raise HTTPException(status_code=400, detail="User not found. Invalid UserID")
+  if username:
+      same_username = db.query(User).filter(User.username == username).first  ()
+      if same_username and same_username.id != user.id:
+          raise HTTPException(status_code=400, detail="Invalid username")
       user.username = username
-    db.commit()
-    return user
-  except:
-    raise HTTPException(status_code=500, detail="Internal Server Error, something went wrong")
+  if name:
+      user.name = name
+  db.commit()
+  return user
   
 @router.delete('/{id}')
 def delete_user(id: int, db: Session = Depends(get_db)):
-  try:
-    user = db.query(User).filter(User.id == id).first()
-    if not user:
-      raise HTTPException(status_code=418, detail="User not found. Invalid UserID")
-    db.delete(user)    
-    db.commit()
-    return user
-  except:
-    raise HTTPException(status_code=500, detail="Internal Server Error, something went wrong")
+  user = db.query(User).filter(User.id == id).first()
+  if not user:
+      raise HTTPException(status_code=400, detail="User not found. Invalid UserID")
+  db.delete(user)
+  db.commit()
+  return user
